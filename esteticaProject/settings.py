08d49af78282cb29fd578e62cb88bf586b8a4750
vanemercedes <111691700+vanemercedes@users.noreply.github.com>
2022-10-03 11:46:20 -0500

    Update settings.py

--- esteticaProject/settings.py
+++ esteticaProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@z&$-x1jd3z_oxfd3o-^q&ggz^-&(@y!l5=bbu8l^voic2@r=w'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
